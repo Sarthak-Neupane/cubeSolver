dbb1490493aadedcda89bdaccc1ba99a32970280
Sarthak Neupane <sarthakrocks2004@gmail.com>
2023-11-06 17:29:08 -0600

    added the colors

--- components/threeBackground.vue
+++ components/threeBackground.vue
@@ -28,56 +28,62 @@ const uProgress = ref(0)
 const aspect = computed(() => width.value / height.value)
 
 const gRubiks = new $three.Group()
+const red = new $three.Color(0xff0000)
+const blue = new $three.Color(0x0000ff)
+const green = new $three.Color(0x00ff00)
+const yellow = new $three.Color(0xffff00)
+const orange = new $three.Color(0xffa500)
+const white = new $three.Color(0xffffff)
+const BLACK_COLOR = new $three.Color(0x000000)
 
 
 const cubeThreeFaces = [
-    { x: -1, y: -1, z: -1 },
-    { x: 1, y: -1, z: -1 },
-    { x: -1, y: 1, z: -1 },
-    { x: 1, y: 1, z: -1 },
-    { x: -1, y: -1, z: 1 },
-    { x: 1, y: -1, z: 1 },
-    { x: -1, y: 1, z: 1 },
-    { x: 1, y: 1, z: 1 }
+    {x: -1, y: -1, z: -1, colors: [BLACK_COLOR, orange, BLACK_COLOR, yellow, BLACK_COLOR, white]},
+    { x: 1, y: -1, z: -1, colors: [green, BLACK_COLOR, BLACK_COLOR, yellow, BLACK_COLOR, white] },
+    { x: -1, y: 1, z: -1, colors: [BLACK_COLOR, orange, red, BLACK_COLOR, BLACK_COLOR, white] },
+    { x: 1, y: 1, z: -1, colors: [ green, BLACK_COLOR, red, BLACK_COLOR, BLACK_COLOR, white ] },
+    { x: -1, y: -1, z: 1, colors: [ BLACK_COLOR, orange, BLACK_COLOR, yellow, blue, BLACK_COLOR ] },
+    { x: 1, y: -1, z: 1, colors: [ green, BLACK_COLOR, BLACK_COLOR, yellow, blue, BLACK_COLOR ] },
+    { x: -1, y: 1, z: 1, colors: [ BLACK_COLOR, orange, red, BLACK_COLOR, blue, BLACK_COLOR ] },
+    { x: 1, y: 1, z: 1, colors: [green, BLACK_COLOR, red, BLACK_COLOR, blue, BLACK_COLOR] }
 ]
 
 const cubeTwoFaces = [
-    { x: 0, y: 1, z: 1 },
-    { x: 0, y: -1, z: 1 },
-    { x: 1, y: 0, z: 1 },
-    { x: -1, y: 0, z: 1 },
-    { x: 0, y: 1, z: -1 },
-    { x: 0, y: -1, z: -1 },
-    { x: 1, y: 0, z: -1 },
-    { x: -1, y: 0, z: -1 },
-    { x: 1, y: 1, z: 0 },
-    { x: 1, y: -1, z: 0 },
-    { x: -1, y: 1, z: 0 },
-    { x: -1, y: -1, z: 0 },
+    { x: 0, y: 1, z: 1, colors: [BLACK_COLOR, blue, BLACK_COLOR, red, BLACK_COLOR, BLACK_COLOR] },
+    { x: 0, y: -1, z: 1, colors: [BLACK_COLOR, blue, yellow, BLACK_COLOR, BLACK_COLOR, BLACK_COLOR] },
+    { x: 1, y: 0, z: 1, colors: [BLACK_COLOR, blue, BLACK_COLOR, BLACK_COLOR, BLACK_COLOR, green] },
+    { x: -1, y: 0, z: 1, colors: [BLACK_COLOR, blue, BLACK_COLOR, BLACK_COLOR, orange, BLACK_COLOR] },
+    { x: 0, y: 1, z: -1, colors: [white, BLACK_COLOR, BLACK_COLOR, red, BLACK_COLOR, BLACK_COLOR] },
+    { x: 0, y: -1, z: -1, colors: [white, BLACK_COLOR, yellow, BLACK_COLOR, yellow, BLACK_COLOR] },
+    { x: 1, y: 0, z: -1, colors: [white, BLACK_COLOR, BLACK_COLOR, BLACK_COLOR, BLACK_COLOR, green] },
+    { x: -1, y: 0, z: -1, colors: [white, BLACK_COLOR, BLACK_COLOR, BLACK_COLOR, orange, BLACK_COLOR] },
+    { x: 1, y: 1, z: 0, colors: [BLACK_COLOR, BLACK_COLOR, BLACK_COLOR, red, BLACK_COLOR, green] },
+    { x: 1, y: -1, z: 0, colors: [BLACK_COLOR, BLACK_COLOR, yellow, BLACK_COLOR, BLACK_COLOR, green] },
+    { x: -1, y: 1, z: 0, colors: [BLACK_COLOR, BLACK_COLOR, BLACK_COLOR, red, orange, BLACK_COLOR] },
+    { x: -1, y: -1, z: 0, colors: [BLACK_COLOR, BLACK_COLOR, yellow, BLACK_COLOR, orange, BLACK_COLOR] },
+]
+
+const colors = [
+    white,
+    red,
+    blue,
+    green,
+    yellow,
+    orange,
 ]
 
 const cubeSingleFaces = [
-    { x: 0, y: 0, z: 1, color: 0x0000ff },
-    { x: 0, y: 0, z: -1, color: 0xffffff },
-    { x: 0, y: 1, z: 0, color: 0xff0000 },
-    { x: 0, y: -1, z: 0, color: 0xffff00 },
-    { x: 1, y: 0, z: 0, color: 0x00ff00 },
-    { x: -1, y: 0, z: 0, color: 0xffa500 },
+    { x: 0, y: 0, z: 1, color: blue },
+    { x: 0, y: 0, z: -1, color: white },
+    { x: 0, y: 1, z: 0, color: red },
+    { x: 0, y: -1, z: 0, color: yellow },
+    { x: 1, y: 0, z: 0, color: green },
+    { x: -1, y: 0, z: 0, color: orange },
 ]
 
-
-const colors = [
-    0x000000,
-    0xffffff,
-    0xff0000,
-    0x0000ff,
-    0x00ff00,
-    0xffff00,
-    0xffa500,
-]
 
 const cubeNoFaces = [
-    { x: 0, y: 0, z: 0 },
+    { x: 0, y: 0, z: 0, color: BLACK_COLOR },
 ]
 
 const scene = useScene()
@@ -95,6 +101,7 @@ scene.add(camera)
 
 
 const geometry = new $three.BoxGeometry(1, 1, 1, 20, 20, 20);
+geometry.needsUpdate = true
 const material = new $three.MeshBasicMaterial({
     color: 0x000000,
     side: $three.DoubleSide,
@@ -111,23 +118,71 @@ const buildTheCube = () => {
                 mesh.position.set(i, j, k)
                 gRubiks.add(mesh)
                 scene.add(mesh)
+                const sumOfPositions = i + j + k
+                if (i === 0 && j === 0 && k === 0) continue
+                if (i !== 0 && j !== 0 && k !== 0) {
+                    const findExactCube = cubeThreeFaces.find((e) => e.x === i && e.y === j && e.z === k)
+                    if(findExactCube){
+                        mesh.material = [
+                            new $three.MeshBasicMaterial({ color: findExactCube.colors[0] }),
+                            new $three.MeshBasicMaterial({ color: findExactCube.colors[1] }),
+                            new $three.MeshBasicMaterial({ color: findExactCube.colors[2] }),
+                            new $three.MeshBasicMaterial({ color: findExactCube.colors[3] }),
+                            new $three.MeshBasicMaterial({ color: findExactCube.colors[4] }),
+                            new $three.MeshBasicMaterial({ color: findExactCube.colors[5] }),
+                        ]
+                    }
+                }
+                if (sumOfPositions === 2 || sumOfPositions === -2 || sumOfPositions === 0) {
+                    const findExactCube = cubeTwoFaces.find((e) => e.x === i && e.y === j && e.z === k)
+                    if (findExactCube) {
+                        mesh.material = [
+                            new $three.MeshBasicMaterial({ color: findExactCube.colors[5] }),
+                            new $three.MeshBasicMaterial({ color: findExactCube.colors[4] }),
+                            new $three.MeshBasicMaterial({ color: findExactCube.colors[3] }),
+                            new $three.MeshBasicMaterial({ color: findExactCube.colors[2] }),
+                            new $three.MeshBasicMaterial({ color: findExactCube.colors[1] }),
+                            new $three.MeshBasicMaterial({ color: findExactCube.colors[0] }),
+                        ]
+                    }
+                }
             }
         }
     }
     console.log(scene)
 }
 
-const colorCenter = ()=>{
-    cubeSingleFaces.forEach((e, i)=>{
-        scene.children.forEach((child)=>{
+const colorCenter = () => {
+    cubeSingleFaces.forEach((e, i) => {
+        scene.children.forEach((child) => {
             if (!child.isMesh) return
-            if(child.position.x === e.x && child.position.y === e.y && child.position.z === e.z){
+            if (child.position.x === e.x && child.position.y === e.y && child.position.z === e.z) {
                 child.material.color.set(e.color)
             }
         })
     })
     console.log(scene)
 }
+
+const colorEdges = () => {
+    // cubeThreeFaces.forEach((e, i) => {
+    //     scene.children.forEach((child) => {
+    //         if (!child.isMesh) return
+    //         if (child.position.x === e.x && child.position.y === e.y && child.position.z === e.z) {
+    //             child.material = [
+    //                 new $three.MeshBasicMaterial({ color: BLACK_COLOR }),
+    //                 new $three.MeshBasicMaterial({ color: BLACK_COLOR }),
+    //                 new $three.MeshBasicMaterial({ color: BLACK_COLOR }),
+    //                 new $three.MeshBasicMaterial({ color: BLACK_COLOR }),
+    //                 new $three.MeshBasicMaterial({ color: BLACK_COLOR }),
+    //                 new $three.MeshBasicMaterial({ color: BLACK_COLOR }),
+    //             ]
+    //         }
+    //     })
+    // })
+}
+
+
 
 
 const addLights = () => {
@@ -169,6 +224,7 @@ const setRenderer = () => {
     addLights()
     buildTheCube()
     colorCenter()
+    colorEdges()
     // loadModel()
 }
 
